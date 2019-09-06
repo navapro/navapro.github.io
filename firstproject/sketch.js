5fdc3fab834dc66896e46d1ce0634dab0858af4d
navapro <45469562+navapro@users.noreply.github.com>
2019-09-06 14:11:40 -0600

    6th

--- firstproject/sketch.js
+++ firstproject/sketch.js
@@ -5,13 +5,29 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let redamount = 4;
+let redchangeamount = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-  background(220);
-  fill(0);
-  ellipse(mouseX, mouseY, 100, 100)
+  fill(redamount,0,225);
+  noStroke();
+  ellipse(mouseX, mouseY, 100, 100);
+  
+  redamount += redchangeamount;
+
+if (redamount >= 255){
+  redchangeamount *= -1;
+}
+
+if (redamount <= 2){
+  redchangeamount *= -1;
+}
+
+console.log(redamount);
+
 }
