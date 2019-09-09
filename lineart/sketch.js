bb9cd4d774348e8146115717427544e541168ecf
navapro <45469562+navapro@users.noreply.github.com>
2019-09-09 14:11:51 -0600

    line art start

---
+++ lineart/sketch.js
@@ -0,0 +1,18 @@
+// Line art demo
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  line(mouseX, mouseY, pmouseX, pmouseY);
+
+}
