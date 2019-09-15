54b29f93ee2d6eb89e633cb0efe3206831a74116
navapro <45469562+navapro@users.noreply.github.com>
2019-09-15 00:42:56 -0600

    added images

--- imageslider/sketch.js
+++ imageslider/sketch.js
@@ -6,15 +6,23 @@
 // - describe what you did to take this project "above and beyond"
 
 let img1;
+let img2;
+let img3;
+let img4;
+let img5;
 
 function preload(){
-  ball = loadImage("assets/bcg-0.jpg");
+  img1 = loadImage("assets/bcg-0.jpg");
+  img2 = loadImage("assets/bcg-1.jpg");
+  img3 = loadImage("assets/bcg-2.jpg");
+  img4 = loadImage("assets/bcg-3.jpg");
+  img5 = loadImage("assets/bcg-4.jpg");
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-
+  image(img1, windowWidth /6,windowHeight/6, windowWidth /1.5,windowHeight/1.5)
 }
  
