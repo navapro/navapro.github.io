9e20b8f6ddf650400bafa87a3f80f4d4731805aa
navapro <45469562+navapro@users.noreply.github.com>
2019-09-21 01:42:05 -0600

    added more comments

--- imageslider/sketch.js
+++ imageslider/sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// define all the variables.
+// Define all the variables.
 let img1;
 let img2;
 let img3;
@@ -25,7 +25,7 @@ let dnext;
 let nextandprevwidth;
 let sound;
 
-// preload all the images and sounds.
+// Preload all the images and sounds.
 function preload() {
   sound = loadSound('assets/sound.mp3');
   img1 = loadImage("assets/bcg-0.jpg");
@@ -37,7 +37,7 @@ function preload() {
   prev = loadImage("assets/prev.ico");
 }
 
-// set the canvas size and image height and width.
+// Set the canvas size and image height and width.
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
@@ -45,39 +45,39 @@ function setup() {
   imagewidth = windowWidth / 1.5;
   nextandprevwidth = windowHeight / 11
 
-  // place the images in an array.
+  // Place the images in an array.
   img = new Array(img1, img2, img3, img4, img5);
 }
 function draw() {
 
-  // setting the background to white
+  // Setting the background to white
   background(255);
 
-  // setting image mode to CENTER and height width and X and Y position for all the images.
+  // Setting image mode to CENTER and height width and X and Y position for all the images.
   imageMode(CENTER);
   image(img[counter], width / 2, height / 2, imagewidth, imageheight);
   nextimage = image(next, width / 1.1, height / 2, height / 5, height / 5);
   previmage = image(prev, width / 12, height / 2, height / 5, height / 5);
 }
 
-// checking if  a key is pressed.
+// Checking if  a key is pressed.
 function keyPressed() {
 
-  //check if the key is right arrow then slide to the next image.
+  // Check if the key is right arrow then slide to the next image.
   if (keyCode === 39) {
     slideimageforward();
   }
 
-  // check if the key is left arrow then slide to the previous image.
+  // Check if the key is left arrow then slide to the previous image.
   if (keyCode === 37) {
     slideimagebackwards();
   }
 }
 
-// checking if mouse  is pressed.
+// Checking if mouse  is pressed.
 function mousePressed() {
 
-  // calculating the distance between mouse pointer and the center of the next and previous image.
+  // Calculating the distance between mouse pointer and the center of the next and previous image.
   dnext = dist(mouseX, mouseY, width / 1.1, height / 2);
   dprev = dist(mouseX, mouseY, width / 12, height / 2);
   
@@ -92,25 +92,25 @@ function mousePressed() {
   }
 }
 
-// checking to see if mouse wheel is turned.
+// Checking to see if mouse wheel is turned.
 function mouseWheel(event) {
 
-  //finding the value of mouse wheel movement.
+  // Finding the value of mouse wheel movement.
   dy = event.delta;
 
-  // checking if mouse wheel movement is positive and zooming out if it is.
+  // Checking if mouse wheel movement is positive and zooming out if it is.
   if (dy > 0) {
     zoomimg_out();
   }
 
-  // checking if mouse wheel movement is negetive and zooming in if it is.
+  // Checking if mouse wheel movement is negetive and zooming in if it is.
   else if (dy < 0) {
     zoomimg_in();
   }
 }
 
 
-// dividing image height and width to zoom out when the function is called.
+// Dividing image height and width to zoom out when the function is called.
 function zoomimg_out() {
 
   imageheight /= 1.1
@@ -118,7 +118,7 @@ function zoomimg_out() {
 
 }
 
-// multiplying image height and width to zoom in when the function is.
+// Multiplying image height and width to zoom in when the function is.
 function zoomimg_in() {
 
   imageheight *= 1.1
@@ -126,24 +126,36 @@ function zoomimg_in() {
 
 }
 
-
-
+// Run setup when window is resized.
 function windowResized() {
   setup();
 }
 
+// Swicth to the next image by adding 1 to counter.
 function slideimageforward() {
   counter++;
-  sound.play();
+ 
+  // If counter is greater than 4, it is outside the array and set couter back to 0.
   if (counter > 4) {
     counter = 0;
   }
+
+  // Play clicking sound when image switches.
+  sound.play();
+
+  
 }
+
+// Swicth to the previous image by subtracting 1 from counter.
 function slideimagebackwards() {
   counter--;
-  sound.play();
+ 
+   // If counter is less than 0, it is outside the array and set couter back to 4.
   if (counter < 0) {
     counter = 4;
   }
+  
+  // Play clicking sound when image switches.
+  sound.play();
 }
 
