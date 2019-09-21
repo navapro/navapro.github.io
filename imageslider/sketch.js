e5779e8fd930de736b2df24df242a958ed1cfa1a
navapro <45469562+navapro@users.noreply.github.com>
2019-09-20 21:53:05 -0600

    added coments

--- imageslider/sketch.js
+++ imageslider/sketch.js
@@ -6,7 +6,6 @@
 // - describe what you did to take this project "above and beyond"
 
 // define all the variables.
-
 let img1;
 let img2;
 let img3;
@@ -27,8 +26,7 @@ let nextandprevwidth;
 let sound;
 
 // preload all the images and sounds.
-
-function preload(){
+function preload() {
   sound = loadSound('assets/sound.mp3');
   img1 = loadImage("assets/bcg-0.jpg");
   img2 = loadImage("assets/bcg-1.jpg");
@@ -40,86 +38,112 @@ function preload(){
 }
 
 // set the canvas size and image height and width.
-
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  imageheight = windowHeight/1.5;
-  imagewidth = windowWidth /1.5;
-  nextandprevwidth = windowHeight/11
 
-// place the images in an array.
-    
-img = new Array(img1,img2,img3,img4,img5);
+  createCanvas(windowWidth, windowHeight);
+  imageheight = windowHeight / 1.5;
+  imagewidth = windowWidth / 1.5;
+  nextandprevwidth = windowHeight / 11
+
+  // place the images in an array.
+  img = new Array(img1, img2, img3, img4, img5);
 }
-
 function draw() {
+
+  // setting the background to white
   background(255);
+
+  // setting image mode to CENTER and height width and X and Y position for all the images.
   imageMode(CENTER);
-  image(img[counter], width /2,height/2, imagewidth,imageheight);
-  nextimage = image(next, width /1.1, height/2, height/5,height/5);
-  previmage = image(prev, width /12, height/2, height/5,height/5);
-  
+  image(img[counter], width / 2, height / 2, imagewidth, imageheight);
+  nextimage = image(next, width / 1.1, height / 2, height / 5, height / 5);
+  previmage = image(prev, width / 12, height / 2, height / 5, height / 5);
 }
 
+// checking if  a key is pressed.
+function keyPressed() {
 
-function keyPressed(){
-  if (keyCode === 39){
+  //check if the key is right arrow then slide to the next image.
+  if (keyCode === 39) {
     slideimageforward();
   }
-  if (keyCode === 37){
-   slideimagebackwards();
+
+  // check if the key is left arrow then slide to the previous image.
+  if (keyCode === 37) {
+    slideimagebackwards();
   }
 }
 
-function mouseWheel(event) {
-  dy = event.delta;
-  if (dy>0){
-    zoomimg();
-   }
-  else if (dy<0){
-     zoomimg_out();
-  }
-}
-
-function zoomimg(){
- 
-  imageheight/= 1.1
-  imagewidth /= 1.1
-
-}
-function zoomimg_out(){
- 
-  imageheight*= 1.1
-  imagewidth *= 1.1
-
-}
+// checking if mouse  is pressed.
 function mousePressed() {
-  dnext = dist(mouseX, mouseY, width /1.1, height/2);
-  dprev = dist(mouseX, mouseY, width /12, height/2);
-  if (dnext<nextandprevwidth){
+
+  // calculating the distance between mouse pointer and the center of the next and previous image.
+  dnext = dist(mouseX, mouseY, width / 1.1, height / 2);
+  dprev = dist(mouseX, mouseY, width / 12, height / 2);
+  
+  // Checking if the mouse pointer is inside the next button slide the image if it is.
+  if (dnext < nextandprevwidth) {
     slideimageforward()
   }
-  if (dprev<nextandprevwidth){
+
+  // Checking if the mouse pointer is inside the previous button and slide the image if it is .
+  if (dprev < nextandprevwidth) {
     slideimagebackwards()
   }
 }
+
+// checking to see if mouse wheel is turned.
+function mouseWheel(event) {
+
+  //finding the value of mouse wheel movement.
+  dy = event.delta;
+
+  // checking if mouse wheel movement is positive and zooming out if it is.
+  if (dy > 0) {
+    zoomimg_out();
+  }
+
+  // checking if mouse wheel movement is negetive and zooming in if it is.
+  else if (dy < 0) {
+    zoomimg_in();
+  }
+}
+
+
+// dividing image height and width to zoom out when the function is called.
+function zoomimg_out() {
+
+  imageheight /= 1.1
+  imagewidth /= 1.1
+
+}
+
+// multiplying image height and width to zoom in when the function is.
+function zoomimg_in() {
+
+  imageheight *= 1.1
+  imagewidth *= 1.1
+
+}
+
+
 
 function windowResized() {
   setup();
 }
 
-function slideimageforward(){
-    counter++;
-    sound.play();
-    if (counter>4){
-      counter=0;
-    }
+function slideimageforward() {
+  counter++;
+  sound.play();
+  if (counter > 4) {
+    counter = 0;
   }
-function slideimagebackwards(){
-    counter--;
-    sound.play();
-    if (counter<0){
-      counter=4;
-    }
+}
+function slideimagebackwards() {
+  counter--;
+  sound.play();
+  if (counter < 0) {
+    counter = 4;
   }
+}
 
