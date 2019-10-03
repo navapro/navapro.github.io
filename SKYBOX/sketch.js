a85d9455a672c1dc882498b20f8e24d981eea59c
navapro <45469562+navapro@users.noreply.github.com>
2019-10-03 14:15:16 -0600

    skybox city

--- SKYBOX/sketch.js
+++ SKYBOX/sketch.js
@@ -29,32 +29,29 @@ control = new THREE.OrbitControls(camera);
 control.addEventListener('change',renderer);
 
 let materialArray = [];
+let texture_ft = new THREE.TextureLoader().load( 'assets/met_ft.jpg');
+let texture_bk = new THREE.TextureLoader().load( 'assets/met_bk.jpg');
+let texture_up = new THREE.TextureLoader().load( 'assets/met_up.jpg');
+let texture_dn = new THREE.TextureLoader().load( 'assets/met_dn.jpg');
+let texture_rt = new THREE.TextureLoader().load( 'assets/met_rt.jpg');
+let texture_lf = new THREE.TextureLoader().load( 'assets/met_lf.jpg');
+  
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
 
-textureFt = new THREE.TextureLoader().load("assets/ft.jpg");
-textureBk = new THREE.TextureLoader().load("assets/bk.jpg");
-textureUp = new THREE.TextureLoader().load("assets/up.jpg");
-textureDn = new THREE.TextureLoader().load("assets/dn.jpg");
-textureRt = new THREE.TextureLoader().load("assets/rt.jpg");
-textureLf = new THREE.TextureLoader().load("assets/lf.jpg");
-
-
-
-materialArray.push(new THREE.MeshBasicMaterial({map: textureFt}));
-materialArray.push(new THREE.MeshBasicMaterial({map: textureBk}));
-materialArray.push(new THREE.MeshBasicMaterial({map: textureUp}));
-materialArray.push(new THREE.MeshBasicMaterial({map: textureDn}));
-materialArray.push(new THREE.MeshBasicMaterial({map: textureRt}));
-materialArray.push(new THREE.MeshBasicMaterial({map: textureLf}));
-
-skyBoxGeo = new THREE.BoxGeometry(10000,10000,10000);
-let skyBox = new THREE.Mesh(skyBoxGeo,materialArray);
-scene.add(skyBox);
-
+for (let i = 0; i < 6; i++)
+materialArray[i].side = THREE.BackSide;
+let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
+let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+scene.add( skybox );  
 animate();
 }
-
 function animate() {
-  renderer.render(scene,camera);
-  requestAnimationFrame(animate);
+renderer.render(scene,camera);
+requestAnimationFrame(animate);
 }
 init();
