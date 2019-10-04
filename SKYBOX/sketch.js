56baedfd6538cb0a591dac9a46d9f17c2f42b2ff
navapro <45469562+navapro@users.noreply.github.com>
2019-10-03 19:16:34 -0600

    nn

--- SKYBOX/sketch.js
+++ SKYBOX/sketch.js
@@ -17,8 +17,9 @@ let windowHeight = window.innerHeight;
 function init(){
 scene = new THREE.Scene();
 
-camera = new THREE.PerspectiveCamera(55, windowWidth/windowHeight,45, 30000);
+camera = new THREE.PerspectiveCamera(55, windowWidth/windowHeight,45, 300000);
 camera.position.set(-900,-200,-900);
+
 
 renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(windowWidth,windowHeight);
