22205c567af747564e2b74c425d0286f96a92971
navapro <45469562+navapro@users.noreply.github.com>
2019-10-09 14:11:34 -0600

    background gif

---
+++ fighte/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function preload() {}
+  img2 = loadImage("assets/bcg-1.jpg");
+
+  function setup() {
+  background(0);
+}
+
+function draw() {
+ 
+}
