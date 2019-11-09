5a683488dc79a2e5121f040d089f18c90c6f209d
navapro <45469562+navapro@users.noreply.github.com>
2019-11-09 13:37:34 -0600

    200

--- gridproject/sketch.js
+++ gridproject/sketch.js
@@ -2,14 +2,17 @@
 let rectX = 0;
 let rectY = 0;
 let box;
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
   box = new Run(rectX,rectY); 
+
 }
 
 function draw() {
+  background(0,0,0,20)
   box.show();
-  
+  box.gravity();
 }
 
 class Run { 
@@ -24,5 +27,9 @@ class Run {
     noStroke();
     rect(floor(this.x),floor(this.y),100,100);
   }
-
+  gravity() {
+    if (this.y < windowHeight / 1.2) {
+        this.y += 4;
+    }
+}
 }
