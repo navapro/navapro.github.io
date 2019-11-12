59ed244ec7dd4d2acce9d510901597b961683a8e
navapro <45469562+navapro@users.noreply.github.com>
2019-11-12 13:59:21 -0600

    firworks

--- fireworks/sketch.js
+++ fireworks/sketch.js
@@ -1,47 +1,65 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+/ OOP Fireworks
 
-let george;
+let theFireworks = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  george = new Walker();
+  angleMode(DEGREES);
 }
 
 function draw() {
- george.display();
- george.move();
-}
-class Walker{
-  constructor(name){
-    this.x = width/2;
-    this.y = height/2;
-    this.fillColor = color (random(255),random(255),random(255));
-    this.stepSize =6;
-    this.radius = 3;
-  }
-display(){
-  fill(this.fillColor);
-  noStroke();
-  circle(this.x,this.y,this,this.radius*2);
-}
-move(){
-  let choice = random(100);
-  if (choice <25){
-    this.y -=this.stepSize;
-  }
-  if (choice <50){
-    this.y +=this.stepSize;
-  }
-  if (choice <75){
-    this.x -=this.stepSize;
-  }
-  else{
-    this.x +=this.stepSize;
+  background(0);
+  for (let i = theFireworks.length - 1; i >= 0; i--) {
+    theFireworks[i].move();
+    if (theFireworks[i].isDone()) {
+      theFireworks.splice(i, 1);
+    }
+    else {
+      theFireworks[i].display();
+    }
   }
 }
+
+function mousePressed() {
+  for (let i = 0; i < 100; i++) {
+    //make a circle
+    let xDir = map(cos(i*4), -1, 1, -3, 3);
+    let yDir = map(sin(i*4), -1, 1, -3, 3);
+    
+    //don't let it be perfectly circular...
+    xDir += random(-0.5, 0.5);
+    yDir += random(-0.5, 0.5);
+
+    let myFirework = new Particle(mouseX, mouseY, xDir, yDir, 5);
+    theFireworks.push(myFirework);
+  }
+}
+
+class Particle {
+  constructor(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.alpha = 255;
+    this.gravity = 0.05;
+  }
+
+  display() {
+    noStroke();
+    fill(255, 0, 0, this.alpha);
+    circle(this.x, this.y, this.radius * 2);
+  }
+
+  move() {
+    this.dy += this.gravity;
+    this.x += this.dx;
+    this.y += this.dy;
+    this.alpha -= 1;
+  }
+
+  isDone() {
+    return this.alpha <= 0;
+  }
 }
