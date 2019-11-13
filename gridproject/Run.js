3cfb34d8d02aff23273ea8156186ec38509dcecc
navapro <45469562+navapro@users.noreply.github.com>
2019-11-12 18:12:03 -0600

    comented run

--- gridproject/Run.js
+++ gridproject/Run.js
@@ -1,35 +1,44 @@
+// create a class and assign variables.
 class Run {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
+
+  // set the fillcolor, players x and y.
   show() {
 
     fill('#FF0000');
     noStroke();
     cx = this.x;
     cy = this.y;
+
+    // find which row and column the player is in.
     playerY = floor(cy / cellHeight);
     playerX = floor(cx / cellWidth);
 
 
   }
+  
+  // add 20 to y position to simulate gravity.
   gravity() {
     if (this.y < windowHeight / 1.358) {
       this.y += 20;
     }
   }
+
+  // subtract 40 to simulate jumping
   jump() {
     this.y -= 40;
   }
 
+// add x to move forward
   forward() {
     this.x += 20;
   }
+// subtract x to move backward
   backward() {
     this.x -= 20;
   }
-  down() {
-    this.y += 20;
-  }
+  
 }
